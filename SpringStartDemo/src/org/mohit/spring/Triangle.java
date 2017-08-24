36ae68e85a26d2e07959ef292e233a74de3877f1
agarwalmohit43 <agarwalmohit43@gmail.com>
2017-08-24 16:11:33 +0530

    Update with spring3

---
+++ SpringStartDemo/src/org/mohit/spring/Triangle.java
@@ -0,0 +1,36 @@
+package org.mohit.spring;
+
+public class Triangle {
+
+	private Point pointA, pointB, pointC;
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+	
+	public void draw(){
+		System.out.println("Point A -> Width: "+getPointA().getWidth()+" Height: "+getPointA().getHeight());
+		System.out.println("Point B -> Width: "+getPointB().getWidth()+" Height: "+getPointB().getHeight());
+		System.out.println("Point C -> Width: "+getPointC().getWidth()+" Height: "+getPointC().getHeight());
+	}
+}
