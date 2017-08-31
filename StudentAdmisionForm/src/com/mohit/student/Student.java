f421a32fed79023975fcbb14df24dc8522b7ffcf
agarwalmohit43 <agarwalmohit43@gmail.com>
2017-08-31 12:17:17 +0530

    RestAPI Started

--- StudentAdmisionForm/src/com/mohit/student/Student.java
+++ StudentAdmisionForm/src/com/mohit/student/Student.java
@@ -7,22 +7,28 @@ import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties({"studentmobile"})
+@JsonPropertyOrder({"student_name", "studenthobby", "studentmobile", "studentdob", "studentskills", "studentaddress" })
 public class Student {
 
+	@JsonProperty("student_name")
 	@Pattern(regexp="[^0-9]*")
 	private String studentname;
 	
 	@Size(min=2,max=10) @isValidHobby(listOfHobbies="Music|Coding")
 	private String studenthobby;
-	
 	private long studentmobile;
 	
 	@Past
 	private Date studentdob;
 	private ArrayList<String> studentskills;
-	
 	private Address studentaddress;
-	
 	
 	
 	public Address getStudentaddress() {
--- StudentAdmisionForm/src/com/mohit/student/StudentAdmisionController.java
+++ StudentAdmisionForm/src/com/mohit/student/StudentAdmisionController.java
@@ -3,6 +3,7 @@ package com.mohit.student;
 import java.io.IOException;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -15,10 +16,15 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.annotation.JsonFormat.Value;
+import com.sun.org.apache.regexp.internal.recompile;
 
 @Controller
 public class StudentAdmisionController {
@@ -34,7 +40,7 @@ public class StudentAdmisionController {
 	@RequestMapping(value="/getadm",method=RequestMethod.GET)
 	public ModelAndView getAdmissionForm() throws NullPointerException,IOException{
 		
-		String exceptionOccured = "IOE_EXCEPTION";
+		String exceptionOccured = "IOE_EXCEPTIONs";
 		
 		if(exceptionOccured.equalsIgnoreCase("NULL_POINTER")){
 			throw new NullPointerException("NULL_POINTER_EXCEPTION");
@@ -79,7 +85,6 @@ public class StudentAdmisionController {
 		ModelAndView modelAndView = new ModelAndView("AdmissionSucces");
 		return modelAndView;
 	}
-	
 	
 	
 }
